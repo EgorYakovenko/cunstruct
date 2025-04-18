a0c3b55578fafc4be1b24be23f6293bce63bd387
EgorYakovenko <Egor.Yakovenko@ukr.net>
2025-04-19 00:54:04 +0200

    markup header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,27 @@
 import css from "./Header.module.css";
 
 export default function Header() {
-  return <div className={css.header}>Header</div>;
+  return (
+    <div className={css.header}>
+      <nav className={css.nav}>
+        <ul className={css.navList}>
+          <li>
+            <a href="">About Us</a>
+          </li>
+          <li>
+            <a href="">Blog</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+          <li>
+            <a href="">FAQs</a>
+          </li>
+          <li>
+            <a href="">Reviews</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
