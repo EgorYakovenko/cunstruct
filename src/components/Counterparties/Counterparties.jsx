241ae2f7471adbcc142740c04de7ffccbd3c65ce
EgorYakovenko <Egor.Yakovenko@ukr.net>
2025-04-27 22:24:51 +0200

    add marcup in add project

--- src/components/Counterparties/Counterparties.jsx
+++ src/components/Counterparties/Counterparties.jsx
@@ -1,39 +1,59 @@
+import { BsPersonVcard } from "react-icons/bs";
 import { MdOutlineEngineering } from "react-icons/md";
+import { GrUserWorker } from "react-icons/gr";
 import css from "./Counterparties.module.css";
 
 export default function Counterparties() {
   return (
     <>
-      <ul>
+      <ul className={css.counterpartiesList}>
         <li className={css.counterpartiesItem}>
           <div className={css.cardContainer}>
-            <MdOutlineEngineering />
+            <GrUserWorker className={css.icon} />
             <div>
-              <p>Исполнители</p>
-              <span>
+              <p className={css.role}>Исполнители</p>
+              <span className={css.description}>
                 Контролировать работу членов команды, которым поручены различные
                 задачи.
               </span>
             </div>
           </div>
 
-          <a href="">Посмотреть детали</a>
+          <a className={css.link} href="">
+            Посмотреть детали
+          </a>
         </li>
-        <li>
+        <li className={css.counterpartiesItem}>
           <div className={css.cardContainer}>
-            <MdOutlineEngineering />
+            <MdOutlineEngineering className={css.icon} />
             <div>
-              <p>Подрядчики</p>
-              <span>
+              <p className={css.role}>Подрядчики</p>
+              <span className={css.description}>
                 Управление компаниями и лицами, ответственными за реализацию
                 проекта.
               </span>
             </div>
           </div>
 
-          <a href="">Посмотреть детали</a>
+          <a className={css.link} href="">
+            Посмотреть детали
+          </a>
         </li>
-        <li></li>
+        <li className={css.counterpartiesItem}>
+          <div className={css.cardContainer}>
+            <BsPersonVcard className={css.icon} />
+            <div>
+              <p className={css.role}>Поставщики</p>
+              <span className={css.description}>
+                Отслеживайте поставщиков материалов и оборудования.
+              </span>
+            </div>
+          </div>
+
+          <a className={css.link} href="">
+            Посмотреть детали
+          </a>
+        </li>
       </ul>
     </>
   );
--- src/page/AddProjectPage/AddProjectPage.jsx
+++ src/page/AddProjectPage/AddProjectPage.jsx
@@ -18,7 +18,9 @@ export default function AddProjectPage() {
       <div className={css.upload}>
         <UploadDocuments />
       </div>
-      <Counterparties />
+      <div className={css.counterparties}>
+        <Counterparties />
+      </div>
     </>
   );
 }
